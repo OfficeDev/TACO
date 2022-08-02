1f9572d907bda678b4b2afd8a9b15a619167d1a6
JohanVeldhuis <johan.veldhuis@microsoft.com>
2022-08-02 14:51:27 +0200

    Teams PS module version correction

--- Deployment/deploy.ps1
+++ Deployment/deploy.ps1
@@ -5,7 +5,7 @@ Param (
     [parameter(mandatory = $true)] [ValidateSet('Australia Central','Australia East','Australia Southeast','Brazil South','Canada Central','Canada East','Central India','Central US','East Asia','East US 2','East US','France Central','Germany West Central','Japan East','Japan West','Korea Central','Korea South','North Central US','North Europe','Norway East','South Africa North','South Central US','South India','Southeast Asia','Sweden Central','Switzerland North','UAE North','UK South','UK West','West Central US','West Europe','West India','West US 2','West US 3','West US')] [string]$location,                   # Location (region) where the Azure resource are deployed
     [parameter(mandatory = $true)] [string]$serviceAccountUPN,                          # AzureAD Service Account UPN
     [parameter(mandatory = $true)] $serviceAccountSecret,                        # AzureAD Service Account password
-    [parameter(mandatory = $false)] $teamsPSModuleVersion = "4.3.0",              # Microsoft Teams PowerShell module version
+    [parameter(mandatory = $false)] $teamsPSModuleVersion = "4.4.1",              # Microsoft Teams PowerShell module version
     [parameter(mandatory = $false)] $subscriptionID               # Microsoft Azure Subscription id  
 )
 
