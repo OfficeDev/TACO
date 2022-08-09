3c701fc06e407da5036fd06ec81e1341a9aa38df
JohanVeldhuis <johan.veldhuis@microsoft.com>
2022-08-09 16:16:13 +0200

    Update JSON-Schemas
    Remove Shared from the *SharedVoicemailTarget and *SharedVoicemailTranscription to fix an issue with the workflows.
    Updated incorrect description Set-CallQueueOverFlowAction

--- FunctionApp/Modules/JSON-Schemas/JSON-Schemas.psm1
+++ FunctionApp/Modules/JSON-Schemas/JSON-Schemas.psm1
@@ -48,14 +48,14 @@ Switch ($schemaName) {
                     "60"
                 ]
             },    			
-            "TimeoutSharedVoicemailTarget": {
+            "TimeoutVoicemailTarget": {
                 "type": "string",
                 "title": "Specifies the O365 group where the voicemail needs to be delivered",
                 "examples": [
                     "it-helpdesk@contoso.com"
                 ]
             },             
-            "TimeoutSharedVoiceMailTranscription": {
+            "TimeoutVoiceMailTranscription": {
                 "type": "boolean",
                 "title": "Specifies if voicemail transcription is enabled or not",
                 "examples": [
@@ -92,7 +92,7 @@ Switch ($schemaName) {
 'Set-CallQueueOverFlowAction' { Return @'
     {
         "type": "object",
-        "title": "Set-CallQueueTimeOutAction API JSON body definition",  
+        "title": "Set-CallQueueOverFlowAction API JSON body definition",  
         "required": [
             "Identity"
         ],
@@ -129,14 +129,14 @@ Switch ($schemaName) {
                     "10"
                 ]
             }, 			
-            "OverflowSharedVoicemailTarget": {
+            "OverflowVoicemailTarget": {
                 "type": "string",
                 "title": "Specifies the O365 group where the voicemail needs to be delivered",
                 "examples": [
                     "it-helpdesk@contoso.com"
                 ]
             },              
-            "OverflowSharedVoicemailTranscription": {
+            "OverflowVoicemailTranscription": {
                 "type": "boolean",
                 "title": "Specifies voice transcription should be on or off",
                 "examples": [
